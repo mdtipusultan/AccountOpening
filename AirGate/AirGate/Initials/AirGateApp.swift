4318a92a7b87e15011725948279a3f881800b525
Md. Tipu <tipu.diu.cse@gmail.com>
2024-11-17 16:09:07 -0800

    app initialization
    app initialization

---
+++ AirGate/AirGate/Initials/AirGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirGateApp.swift
+//  AirGate
+//
+//  Created by Tipu on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AirGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchScreenView()
+        }
+    }
+}
---
+++ AirGate/AirGate/LaunchScreen/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  AirGate
+//
+//  Created by Tipu on 11/17/24.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Image("top_image")
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
